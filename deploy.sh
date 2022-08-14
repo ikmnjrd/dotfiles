d13f36d1a245f06ce5113f8333875796a432b017
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-08-14 10:36:17 +0900

    add: alacritty.yml

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@
 #     -not -path "*/org/*"))
 dotfiles+=("$(pwd)/.zshrc")
 dotfiles+=("$(pwd)/.tmux.conf")
+dotfiles+=("$(pwd)/.alacritty.yml")
 dotfiles+=("$(pwd)/vscode/keybindings.json")
 dotfiles+=("$(pwd)/vscode/settings.json")
 
@@ -25,6 +26,7 @@ dotfiles+=("$(pwd)/vscode/settings.json")
 #     -not -name "*.swo"))
 home_dotsfile+=("$HOME/.zshrc")
 home_dotsfile+=("$HOME/.tmux.conf")
+home_dotsfile+=("$HOME/.alacritty.yml")
 ## vscode
 if [ "$(uname)" == 'Darwin' ]; then
     home_dotsfile+=("$HOME/Library/Application Support/Code/User/keybindings.json")
